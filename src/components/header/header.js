c589f8fa7cec1b579749b83d7abdf46b51ddbb61
Oluwatosin Akinyele <oluwatosinakinyele@Oluwatosins-MacBook-Pro.local>
2019-05-16 09:05:01 +0100

    feature(create landingpage) - Implement landing page component

---
+++ src/components/header/header.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+import './header.css'
+class Header extends Component{
+    render(){
+      return (
+        <div className='header'>
+        <div className="container">
+            <div id="branding">
+                <h1><img src="img/thumb1.png" width="30px"></img><span className="highlight">Politico</span> </h1>
+            </div>
+            <nav>
+                <ul>
+                    <li><i className="fas fa-home"></i><a href="login.html"> Login</a></li>
+                    <li><i className="fas fa-plus-circle"></i><a href="signup.html"> Sign Up</a></li>
+                    </ul>
+            </nav>
+        </div>
+        </div>
+      )
+    }
+   };
+   export default Header;
---
+++ src/components/main/main.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import Showcase from './showcase';
+import Newsletter from './newsletter';
+import Boxes from './boxes';
+import './main.css'
+class Main extends Component{
+    render(){
+      return (
+        <div className='main'>
+        <Showcase/>
+        <Newsletter/>
+        <Boxes/>
+        </div>
+      )
+    }
+   };
+   export default Main;
